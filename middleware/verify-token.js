9437d5ba118fd3ee11c4f410da00ceb53d4c270c
zeemohamed7 <zainabshmohamed7@gmail.com>
2025-02-09 20:39:51 +0300

    create verifyToken middleware

---
+++ middleware/verify-token.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        // Assign decoded payload to req.user
+        req.user = decoded.payload
+
+        next()
+    } catch(err) {
+        res.status(401).json({ err: 'Invalid token.'})
+    }
+}
+
+module.exports = verifyToken
